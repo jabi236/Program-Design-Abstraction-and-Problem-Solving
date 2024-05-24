7770c40de6e6ae8c5cadaa1d76a7b3e962dca0e7
jabi236 <123780845+jabi236@users.noreply.github.com>
2024-05-24 17:33:57 -0400

    Added lab 5 and test ended message

--- main.cpp
+++ main.cpp
@@ -404,13 +404,131 @@ void lab4(){
     return;
 };
 
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+//                                                              Lab 5
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+const int LAB5_MAX_STUDENTS = 20;
+const int LAB5_MAX_SCORES = 5;
+
+struct lab5_student{
+    string first;
+    string last;
+    float avg;
+    int scores[LAB5_MAX_SCORES];
+    int numScores;
+};
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+// Function: Read
+// Date: 5/23/24
+// Description: Read in student members and allocate them to array of student structs
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+void lab5_read(lab5_student *s, int & numStu, string filename){
+
+    int numScores_input;
+
+    ifstream f;
+    f.open(filename);
+    if(f.fail()){
+        cout << "Unable to open input file\n";
+        numStu = 0;
+        return;
+    }
+    f >> numStu >> numScores_input; 
+
+    for(int i = 0; i < numStu; i++){
+        f >> s[i].first >> s[i].last;
+        for(int j = 0; j < numScores_input; j++){
+            f >> s[i].scores[j];
+        }
+        s[i].numScores = numScores_input;
+    }
+
+    f.close();
+
+    return;
+};
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+// Function: Calculate Averages
+// Date: 5/23/24
+// Description: Get the average of each students scores and store them in their structure
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+void lab5_calcAverages(lab5_student *s, int numStu){
+    for(int i = 0; i < numStu; i++){
+        float totScores = 0;
+        for(int j = 0; j < s[i].numScores; j++){
+            totScores += s[i].scores[j];
+        }
+        s[i].avg = totScores / s[i].numScores;
+    }
+    return;
+};
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+// Function: Print
+// Date: 5/23/24
+// Description: Prints head and the data for each student on one line
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+void lab5_print(lab5_student *s, int numStu){
+    cout << setw(16) << left << "First" << setw(16) << left << "Last" << setw(7) << left << "Avg"
+         << setw(4) << left << "S1" << setw(4) << left << "S2" << setw(4) << left << "S3" << setw(4) << left << "S4" << setw(4) << left << "S5" 
+         << "\n--------------- --------------- ------ --- --- --- --- ---";
+    for(int i = 0; i < numStu; i++){
+        cout << endl << setw(15) << left << s[i].first << " " << setw(15) << left << s[i].last << " " << fixed << setprecision(2) << setw(6) << right << s[i].avg;
+        for(int j = 0; j < s[i].numScores; j++){
+            cout << " " << setw(3) << right << s[i].scores[j];
+        }
+    }
+    cout << endl;
+};
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+// Function: Search
+// Date: 5/23/24
+// Description: Search for student using first and last name and return index of that student if found. Otherwise return -1.
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+int lab5_search(lab5_student *s, int numStu, string fname, string lname){
+    int found = -1;
+
+    for(int i =0; i < numStu; i++){
+        if( (s[i].first == fname) && (s[i].last == lname) ){
+            found = i;
+        }
+    }
+
+    return found;
+};
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 // Function: Lab 5
 // Date: 5/16/24
-// Description: 
+// Description: a program that reads data from a file on students and their project scores.
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 void lab5(){
-    cout << "This lab is currently not available\n";
+    lab5_student s[LAB5_MAX_STUDENTS];
+    int numStu;
+    string sFirst, sLast;
+    string fn = "students.txt";
+
+    lab5_read(s, numStu, fn);
+    lab5_calcAverages(s, numStu);
+    lab5_print(s, numStu);
+
+    cout << "Enter First and Last to search (or 0 0): ";
+    cin >> sFirst >> sLast;
+    while(sFirst != "0"){
+        int ndx = lab5_search(s, numStu, sFirst, sLast);
+        if(ndx == -1){
+            cout << "Not found\n";
+        }
+        else{
+            cout << "Average is " << s[ndx].avg << endl;
+        }
+        cout << "Enter First aand Last to search (or 0 0): ";
+        cin >> sFirst >> sLast;
+    }
+
+    return;
 };
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
@@ -774,11 +892,13 @@ void testerMode(){
             cin.ignore();
             cout << "Loading \"helloworld\"\n";
             helloworld();
+            cout << "\nEnd of test...\n";
         }
         else if(prog == "lab1"){
             cin.ignore();
             cout << "Loading \"lab1\"...\n";
             lab1();
+            cout << "\nEnd of test...\n";
         }
         else if(prog == "lab2"){
             string ver2 = "";
@@ -789,11 +909,13 @@ void testerMode(){
                 cout << "Loading \"lab2_2018\"...\n";
                 cin.ignore();
                 lab2_2018();
+                cout << "\nEnd of test...\n";
             }
             else if(ver2 == "2021"){
                 cout << "Loading \"lab2_2021\"...\n";
                 cin.ignore();
                 lab2_2021();
+                cout << "\nEnd of test...\n";
             }
             else{
                 cout << "Invalid input. Returning to program select\n";
@@ -808,11 +930,13 @@ void testerMode(){
                 cout << "Loading \"lab3_2018\"...\n";
                 cin.ignore();
                 lab3_2018();
+                cout << "\nEnd of test...\n";
             }
             else if(ver3 == "2021"){
                 cout << "Loading \"lab3_2021\"...\n";
                 cin.ignore();
                 lab3_2021();
+                cout << "\nEnd of test...\n";
             }
             else{
                 cout << "Invalid input. Returning to program select\n";
@@ -822,36 +946,43 @@ void testerMode(){
             cout << "Loading \"lab4\"...\n";
             cin.ignore();
             lab4();
+            cout << "\nEnd of test...\n";
         }
         else if(prog == "lab5"){
             cout << "Loading \"lab5\"...\n";
             cin.ignore();
             lab5();
+            cout << "\nEnd of test...\n";
         }
         else if(prog == "lab6"){
             cout << "Loading \"lab6\"...\n";
             cin.ignore();
             lab6();
+            cout << "\nEnd of test...\n";
         }
         else if(prog == "lab7"){
             cout << "Loading \"lab7\"...\n";
             cin.ignore();
             lab7();
+            cout << "\nEnd of test...\n";
         }
         else if(prog == "lab8"){
             cout << "Loading \"lab8\"...\n";
             cin.ignore();
             lab8();
+            cout << "\nEnd of test...\n";
         }
         else if(prog == "lab9"){
             cout << "Loading \"lab9\"...\n";
             cin.ignore();
             lab9();
+            cout << "\nEnd of test...\n";
         }
         else if(prog == "lab10"){
             cout << "Loading \"lab10\"...\n";
             cin.ignore();
             lab10();
+            cout << "\nEnd of test...\n";
         }
         else if(prog == "proj1"){
             string verP1 = "";
@@ -862,11 +993,13 @@ void testerMode(){
                 cout << "Loading \"proj1_2018\"...\n";
                 cin.ignore();
                 proj1_2018();
+                cout << "\nEnd of test...\n";
             }
             else if(verP1 == "2021"){
                 cout << "Loading \"proj1_2021\"...\n";
                 cin.ignore();
                 proj1_2021();
+                cout << "\nEnd of test...\n";
             }
             else{
                 cout << "Invalid input. Returning to program select\n";
